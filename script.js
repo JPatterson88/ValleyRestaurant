e39961f0d7b74e321ffbd13fba808a6bcfe86e23
JPatterson88 <traverisp@gmail.com>
2021-04-30 13:07:55 -0400

    4/30/21 commit on master

--- script.js
+++ script.js
@@ -14,3 +14,51 @@ setInterval(()=> {
     }
 }
 , 4000);
+
+document.querySelector('.btn.dinner').addEventListener('click', ()=> {
+    document.querySelectorAll('.dinner-target').forEach((item) => {
+        item.classList.toggle('display');
+    })
+})
+
+document.querySelector('.btn.drinks').addEventListener('click', ()=> {
+    document.querySelectorAll('.drinks-target').forEach((item) => {
+        item.classList.toggle('display');
+    })
+})
+
+document.querySelector('.btn.dessert').addEventListener('click', ()=> {
+    document.querySelectorAll('.dessert-target').forEach((item) => {
+        item.classList.toggle('display');
+    })
+})
+    
+    
+    
+
+
+
+document.querySelector('.dinner.close').addEventListener('click', ()=> {
+    document.querySelectorAll('.display').forEach((item) => {
+        item.classList.toggle('display');
+    })
+})
+
+document.querySelector('.drinks.close').addEventListener('click', ()=> {
+    document.querySelectorAll('.display').forEach((item) => {
+        item.classList.toggle('display');
+    })
+})
+
+
+document.querySelector('.dessert.close').addEventListener('click', ()=> {
+    document.querySelectorAll('.display').forEach((item) => {
+        item.classList.toggle('display');
+    })
+})
+
+
+
+
+
+    
